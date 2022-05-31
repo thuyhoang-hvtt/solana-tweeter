287ff31f20c96852079540ee20c75b493e3f6c48
Trong Thuy <thuyhoang.hvtt@gmail.com>
2022-05-31 17:32:44 +0700

    🤡 feat: mock apis

---
+++ app/src/api/fetch-tweets.ts
@@ -0,0 +1,29 @@
+export const fetchTweets = async () => {
+  return [
+    {
+      key: '0x',
+      topic: 'solana',
+      content: 'gm',
+      author: 'B1Af..wtRN',
+      created_at: 'Nov 26, 2021 1:03PM',
+      created_ago: 'just now',
+      timestamp: 1637932864,
+    },
+    {
+      topic: 'no-code',
+      content: 'Octohook.com is awesome!',
+      author: 'BnE7..NRGF',
+      created_at: 'Nov 26, 2021 1:03PM',
+      created_ago: '2 hours ago',
+      timestamp: 1637932864,
+    },
+    {
+      topic: '',
+      content: 'Just setting up my Solana twttr',
+      author: 'B1Af..wtRN',
+      created_at: 'Nov 26, 2021 1:03PM',
+      created_ago: '2 days ago',
+      timestamp: 1637932864,
+    },
+  ];
+};
