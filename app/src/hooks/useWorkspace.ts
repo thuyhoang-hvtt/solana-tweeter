578a1e30d7af8de899456f88a72f994cef6c7087
Trong Thuy <thuyhoang.hvtt@gmail.com>
2022-06-02 12:20:28 +0700

    🍱 chore: update hooks

--- app/src/hooks/useWorkspace.ts
+++ app/src/hooks/useWorkspace.ts
@@ -1,11 +1,13 @@
 import { computed } from 'vue';
-import { useAnchorWallet } from 'solana-wallets-vue';
+import { useWallet, useAnchorWallet } from 'solana-wallets-vue';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Idl, Program, AnchorProvider, Wallet } from '@project-serum/anchor';
 import { SolanaTwitter } from '@src/idl/solana_twitter';
 import { IWorkspace } from '@src/interface';
 import idl from '@src/idl/solana_twitter.json';
 
+const preflightCommitment = 'processed';
+const commitment = 'processed';
 const programId = new PublicKey((idl as Idl).metadata.address);
 let workspace: IWorkspace;
 
@@ -14,18 +16,19 @@ export const useWorkspace = () => workspace;
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
   const connection = new Connection(
-    import.meta.env.RPC_URL ?? 'http://127.0.0.1:8899'
+    import.meta.env.RPC_URL ?? 'http://127.0.0.1:8899',
+    commitment
   );
   const provider = computed(
-    () => new AnchorProvider(connection, wallet.value as Wallet, {})
+    () =>
+      new AnchorProvider(connection, wallet.value as Wallet, {
+        preflightCommitment,
+        commitment,
+      })
   );
   const program = computed(
-    () =>
-      new Program(
-        idl as SolanaTwitter,
-        programId,
-        provider.value
-      ) as Program<SolanaTwitter>
+    // @ts-ignore
+    () => new Program<SolanaTwitter>(idl, programId, provider.value)
   );
 
   workspace = {
